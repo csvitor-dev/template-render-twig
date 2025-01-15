27cadab4f0483d0bbdb86e614c1d64c857b2f081
csvitor-dev <csvitor.dev@gmail.com>
2025-01-15 13:30:19 -0300

    feat: create an array with allow routes

---
+++ templates/router.php
@@ -0,0 +1,16 @@
+<?php
+
+$ROUTES = [
+    '/' => [
+        'file_path' => 'front/pages/home.html.twig',
+        'title' => 'Home',
+    ],
+    '/main-news' =>  [
+        'file_path' => 'front/pages/main-news.html.twig',
+        'title' => 'Main News',
+    ],
+    '/about' => [
+        'file_path' => 'front/pages/about.html.twig',
+        'title' => 'About',
+    ],
+];
