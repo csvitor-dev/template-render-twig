1adf3f43e1682d91bb734269306e55235fcad83e
csvitor-dev <csvitor.dev@gmail.com>
2025-01-15 09:40:35 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template Rendering System | PHP</title>
+</head>
+<body>
+    <?php
+        date_default_timezone_set("America/Sao_Paulo");
+
+        echo "<h1>" . phpversion() . "</h1>";
+    ?>
+</body>
+</html>
